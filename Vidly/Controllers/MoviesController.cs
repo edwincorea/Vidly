2f521a3571a5043c37245da59698103ede8893c5
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-01-20 17:28:32 -0600

    Action parameters.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,5 +12,23 @@ namespace Vidly.Controllers
 
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+
+        }
+
+        // GET: Movies/
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrEmpty(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
