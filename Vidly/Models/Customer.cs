78fb19ae93373001c3a0f98db7d0d170786917ad
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-01-30 15:50:31 -0600

    Changing the model.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -4,5 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        // Navigation property
+        public MembershipType MembershipType { get; set; }
+        // For optimizations, we declare a foreign key. By convention: ClassName+Id
+        public byte MembershipTypeId { get; set; } 
     }
 }
