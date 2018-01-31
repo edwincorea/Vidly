058a38089c7a766dee1366c56650ec61797f8ccd
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-01-31 15:37:10 -0600

    Drop-down Lists.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -6,15 +6,21 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+        
         // Navigation property
         public MembershipType MembershipType { get; set; }
+
         // For optimizations, we declare a foreign key. By convention: ClassName+Id
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; } 
     }
 }
---
+++ Vidly/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class NewCustomerViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        // If we have new properties in domain object (i.e. fullname), we don't want to pollute entity object. In this case, we reuse entity object.
+        public Customer Customer { get; set; }
+    }
+}
