005840f479d0e6c742eed6691367c843ac9cc877
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-01-31 17:31:24 -0600

    Model Binding

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -31,6 +31,12 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            return View();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
