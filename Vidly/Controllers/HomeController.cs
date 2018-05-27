12133afbccbd07774556e2e40b84d853366e2e8b
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-05-27 16:59:34 -0600

    Output cache.

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -1,10 +1,21 @@
 ﻿using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Vidly.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        //[OutputCache(
+        //    Duration = 50, 
+        //    Location = OutputCacheLocation.Server, 
+        //    VaryByParam = "*")]
+        
+        //Disabling caching:
+        [OutputCache(
+            Duration = 0,
+            VaryByParam = "*",
+            NoStore = true)]
         public ActionResult Index()
         {
             return View();
