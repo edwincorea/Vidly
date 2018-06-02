dc2ae7dc1c4edb0f3c5322339bc58a12fc900543
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-06-02 16:04:00 -0600

    Build an API for renting movies..

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
@@ -11,14 +12,13 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
-        public DateTime DateAdded { get; set; }
-
         public Genre Genre { get; set; }
 
         [Display(Name = "Genre")]
         [Required]
         public byte GenreId { get; set; }
 
+        public DateTime DateAdded { get; set; }
 
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
@@ -26,5 +26,7 @@ namespace Vidly.Models
         [Display(Name = "Number in Stock")]
         [Range(1, 20)]
         public byte NumberInStock { get; set; }
+
+        public byte NumberAvailable { get; set; }
     }
 }
