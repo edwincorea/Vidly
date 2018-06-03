3793ae9c17b2fa70b13cc1c8dfe96c1ad42508a8
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-06-02 18:34:55 -0600

    Updating the DOM.

--- Vidly/Controllers/API/MoviesController.cs
+++ Vidly/Controllers/API/MoviesController.cs
@@ -17,10 +17,16 @@ namespace Vidly.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
-        public IHttpActionResult GetMovies()
+        public IHttpActionResult GetMovies(string query = null)
         {
-            var moviesDtos = _context.Movies
-                .Include(m => m.Genre)
+            var moviesQuery = _context.Movies
+                 .Include(m => m.Genre)
+                 .Where(m => m.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+
+            var moviesDtos = moviesQuery
                 .ToList()
                 .Select(Mapper.Map<Movie, MovieDto>);
 
